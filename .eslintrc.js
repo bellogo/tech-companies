b9669b7e4c88075ea584ad6d2588780f61fdd3ff
Ufuoma Ogodo <ufuomaogodo@gmail.com>
2020-08-29 13:55:33 +0100

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true,
+        "node": true,
+        "mocha": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {}
+};
