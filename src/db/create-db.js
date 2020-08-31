52dfd8f157743213449674c2149b6e855e725463
Ufuoma Ogodo <ufuomaogodo@gmail.com>
2020-08-31 07:42:45 +0100

    travis trial

--- src/db/create-db.js
+++ src/db/create-db.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL || process.env.LOCAL_DATABASE_URL
 });
 
 pool.on('connect', () => {
@@ -33,7 +33,7 @@ const createTables = () => {
       console.log(err);
       pool.end();
     });
-}
+};
 
 /**
  * Drop Tables
